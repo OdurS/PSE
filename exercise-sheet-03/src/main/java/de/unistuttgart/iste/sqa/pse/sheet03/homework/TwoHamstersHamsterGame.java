af4cac188abf7a1b308db0be59509a137478a17e
OdurS <odur.schweikert@gmail.com>
2024-11-05 19:45:53 +0100

    Üb3 Au2 b fertig mit "Fehlern"

--- exercise-sheet-03/src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/TwoHamstersHamsterGame.java
+++ exercise-sheet-03/src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/TwoHamstersHamsterGame.java
@@ -1,16 +1,28 @@
 package de.unistuttgart.iste.sqa.pse.sheet03.homework;
 
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
+
 /**
  * Describe the class TwoHamstersHamsterGame here.
  *
- * @author (Your name)
+ * @author Schweikert
  *
  */
 public class TwoHamstersHamsterGame extends InternalTwoHamstersHamsterGame {
 
 	@Override
 	void hamsterRun() {
-		
-		// TODO implement Exercise 2 (b) here
+		tiffany.move(); 
+		controllerTiffany.pickAllGrains();
+		tiffany.turnLeft();
+		controllerTiffany.moveNTimes(2);
+		controllerTiffany.pickAllGrains();
+		controllerTiffany.turnLeftNTimes(3);
+		controllerTiffany.moveNTimes(2);
+		controllerTiffany.pickAllGrains();
+		controllerTiffany.turnLeftNTimes(2);
+		controllerTiffany.moveNTimes(3);
+		controllerTiffany.putAllGrains();
+
 	}
 }
