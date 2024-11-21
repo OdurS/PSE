ba9bfd08edc40e1dd919371cc78d211a2de7e3a8
OdurS <odur.schweikert@gmail.com>
2024-11-21 20:52:48 +0100

    ..

--- exercise-sheet-05-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
+++ exercise-sheet-05-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
@@ -1,18 +1,21 @@
 package de.unistuttgart.iste.sqa.pse.sheet05.homework;
 
+import de.hamstersimulator.objectsfirst.datatypes.Direction;
+import de.hamstersimulator.objectsfirst.datatypes.Location;
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
 import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
 
 /**
  * In this hamster-game {@code paule} gets four new friends.
  *
- * @author (Your name)
+ * @author Schweikert
  * @version (A date or a version number)
  */
 public class HamsterPartyGame extends SimpleHamsterGame {
-
-	/**
-	 * Constructor for the Party Game.
-	 */
+		Hamster adam= new Hamster(game.getTerritory(),new Location(0,0),Direction.EAST,0);
+		Hamster eva= new Hamster(game.getTerritory(),new Location(0,1),Direction.EAST,0);
+		Hamster erik= new Hamster(game.getTerritory(),new Location(4,2),Direction.EAST,0);
+		Hamster lupin= new Hamster(game.getTerritory(),new Location(4,4),Direction.WEST,0);
 	public HamsterPartyGame() {
 		this.loadTerritoryFromResourceFile("/territories/territory-hamsterparty.ter");
 		this.displayInNewGameWindow();
