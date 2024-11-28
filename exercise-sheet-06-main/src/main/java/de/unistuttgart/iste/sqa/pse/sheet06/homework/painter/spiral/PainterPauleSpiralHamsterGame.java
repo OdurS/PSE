03eaa54ac3c5aa2dd6be673748db3973b0c1d8cb
OdurS <odur.schweikert@gmail.com>
2024-11-28 11:14:25 +0100

    Aufgabe 2 b fertig

--- exercise-sheet-06-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/painter/spiral/PainterPauleSpiralHamsterGame.java
+++ exercise-sheet-06-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/painter/spiral/PainterPauleSpiralHamsterGame.java
@@ -5,7 +5,7 @@ import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
 /**
  * Describe the purpose of this class here.
  *
- * @author (Your name)
+ * @author Schweikert
  * @version (a version number or a date)
  */
 public class PainterPauleSpiralHamsterGame extends SimpleHamsterGame {
@@ -24,12 +24,45 @@ public class PainterPauleSpiralHamsterGame extends SimpleHamsterGame {
 	protected void run() {
 		spiral();
 	}
+	private void turnRight() {
+		paule.turnLeft();
+		paule.turnLeft();
+		paule.turnLeft();
+	}
+	private int lengthOfLine = 0;
+	private void paintFirstLineAndGetLength(){
+		while (paule.frontIsClear()) { 
+			paule.putGrain();
+			paule.move();
+			lengthOfLine = lengthOfLine +1;
+		}
+		turnRight();
+
+	}
+	private void PaintLineLength(){
+		for (int i = 0; i < lengthOfLine; i++) {
+			paule.putGrain();
+			paule.move();
+		}
+
+	}
+	private void PaintNextLength(){
+		for (int i = 0; i < 2; i++) {
+			PaintLineLength();
+			turnRight();
+		}
+		lengthOfLine = lengthOfLine -2;
+	}
 
 
 	/**
 	 * TODO insert JavaDoc here.
 	 */
 	private void spiral() {
+			paintFirstLineAndGetLength();
+		while (!paule.mouthEmpty()) { 
+			PaintNextLength();
+		}
 		
 	}
 }
