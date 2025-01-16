841f76e97e1f59998d5c98534c657e21c5b8f01f
OdurS <odur.schweikert@gmail.com>
2025-01-16 19:28:21 +0100

    test

--- exercise-sheet-10-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet10/homework/warehouse/StorageRack.java
+++ exercise-sheet-10-main/src/main/java/de/unistuttgart/iste/sqa/pse/sheet10/homework/warehouse/StorageRack.java
@@ -1,20 +1,23 @@
 package de.unistuttgart.iste.sqa.pse.sheet10.homework.warehouse;
 
-import de.unistuttgart.iste.sqa.pse.sheet10.homework.warehouse.items.StationeryItem;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import de.unistuttgart.iste.sqa.pse.sheet10.homework.warehouse.items.StationeryItem;
 
 /**
  * Represents a warehouse that can hold a fixed number of items.
  * The number of holdable items is defined by the capacity of the storage rack.
  *
- * @author your name
+ * @author Schweikert
  */
 public final class StorageRack {
 	// classinvariants:
 	// capacity > 0;
 	// numberOfItems >= 0;
 	// numberOfItems <= capacity;
-
+	private List<StationeryItem> shelves;
 	private final int capacity;
 	private int numberOfItems;
 	// TODO: Add data structures for exercises part (a) and (c) here.
@@ -31,18 +34,33 @@ public final class StorageRack {
 	 * @throws IllegalArgumentException If capacity is less than 1.
 	 */
 	public StorageRack(final int capacity) {
+		
 		if (capacity <= 0) {
 			throw new IllegalArgumentException("A warehouse must have a minimum capacity of 1.");
 		}
 		this.capacity = capacity;
 		numberOfItems = 0;
 		// TODO initialize data structures for exercises part (a) and (c) here.
+		this.shelves = new ArrayList<>(capacity);
+
+		for (int i = 0; i < capacity; i++) {
+			Optional<Object> a = Optional.empty();
+            //shelves.add(a);  geht nicht
+        }
 	}
 
 	// TODO add documentation here.
-	public void addItem(final StationeryItem stationeryItem) {
-		// TODO implement exercises part (b) and (d) here.
-	}
+	public void addItem(StationeryItem stationeryItem) {
+        for (int i = 0; i < capacity; i++) {
+            if (shelves.get(i).isEmpty()) {
+                shelves.set(i, Optional.of(stationeryItem));
+                System.out.println("Item hinzugefügt an Platz " + i);
+                return; // Beenden, sobald das Item hinzugefügt wurde
+            }
+        }
+        throw new IllegalStateException("Kein freies Fach verfügbar."); // Ausnahme werfen, wenn kein freier Platz vorhanden
+		//d)
+    }
 
 	// TODO add documentation here.
 	public void removeItem(final int compartmentNumber) {
